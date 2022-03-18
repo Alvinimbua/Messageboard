bd32dedabe7ea44ae1d72bb25c5ccd4cb7b06c94
AlvinImbua <bulimua@gmail.com>
2022-03-18 16:22:30 +0300

    first commit in laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,44 @@
+@extends('master')
+
+@section('title', 'Homepage')
+
+@section('content')
+
+Post a message:
+
+
+<form action="/create" method="post">
+    <input type="text" name="title" placeholder="Title">
+    <input type="text" name="content" placeholder="Content">
+    {{csrf_field()}}
+    <button type="submit">Submit</button>
+
+</form>
+
+<br>
+
+    Recent Messages:
+
+
+    <ul>
+     @foreach($messages as $message)
+
+     <li>{{ $message->title}}<br>
+
+     {{ $message->content}}
+
+     <br>
+
+     {{ $message->created_at->diffForHumans()}}
+
+     <br>
+
+     <a href="/message/{{ $message->id }}">View</a>
+
+    </li>
+
+     @endforeach
+
+    </ul>
+
+@endsection
